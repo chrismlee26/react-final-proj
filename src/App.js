074e4b19c63702035d9e8d8a84559782b1a7d1b4
CHRIS_LEE <chrismlee87@gmail.com>
2021-06-29 15:45:54 -0700

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Provider} from 'react-redux'
+
 
 function App() {
   return (
